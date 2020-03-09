7acf0023383d930abd7dcf61eb9186a245336ccf
Filipp <louieandi@gmail.com>
2020-03-09 14:44:47 +0300

    Base project files and settings

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Controles all game processes
+    /// </summary>
+    public class GameManager : MonoBehaviour
+    {
+        
+    }
+}
