1ce30ab6c805039a45cc388c92f03cceb87b8c41
Filipp <louieandi@gmail.com>
2020-03-20 15:09:04 +0300

    Localization

---
+++ Assets/Scripts/Localization/LocalizationManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Assets.Scripts.Localization
+{
+    public enum Language
+    {
+        English = 0,
+        Russian = 1,
+    }
+
+    /// <summary>
+    /// Loads and handles all localized values
+    /// </summary>
+    public class LocalizationManager
+    {
+        public static Language CurrentLanguage = Language.English;
+
+        private static bool isInitialized = false;
+        private static Dictionary<string, List<string>> localization;
+        private static readonly string jsonPath = Path.Combine(Application.dataPath, "Resources", "Localization", "localization.json");
+
+        public static void Initialize()
+        {
+            string text = File.ReadAllText(jsonPath);
+            localization = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(text);
+
+            isInitialized = true;
+        }
+
+        public static string GetLocalizedValue(string key)
+        {
+            if (!isInitialized)
+                Initialize();
+
+            if (localization.TryGetValue(key, out var value))
+            {
+                return value[(int) CurrentLanguage];
+            }
+
+            return key;
+        }
+    }
+}
---
+++ Assets/Scripts/UIManagement/LocalizedText.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Localization;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UIManagement
+{
+    /// <summary>
+    /// Localizes text value of the component it is attached to
+    /// </summary>
+    [RequireComponent(typeof(Text))]
+    public class LocalizedText : MonoBehaviour
+    {
+        private Text text;
+
+        public void Start()
+        {
+            text = GetComponent<Text>();
+            text.text = LocalizationManager.GetLocalizedValue(text.text);
+        }
+    }
+}
